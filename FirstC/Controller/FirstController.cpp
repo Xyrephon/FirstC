2d83c3f18419ad58c717173e6753d56c6504b343
Xyrephon <jtuc1613@740s-pl-115.local>
2017-01-25 09:48:05 -0700

    Added user inputs and some outputs.

--- FirstC/Controller/FirstController.cpp
+++ FirstC/Controller/FirstController.cpp
@@ -13,5 +13,26 @@ using namespace std;
 
 void FirstController :: start()
 {
-    cout << "Hello World!" << endl;
+    cout << "I never used the forbidden words, don't worry about it." << endl;
+    int myAge = 16;
+    cout << "I am " << myAge << " years old." << endl;
+    cout << "\n";
+    
+    specialOutput();
+}
+
+void FirstController :: specialOutput()
+{
+    cout << "How old are you?" << endl;
+    int yourAge;
+    cin >> yourAge;
+    cout << "Oh, so you are " << yourAge << " years old!" << endl;
+    string myName;
+    cin.ignore();
+    cout << "Type in your name" << endl;
+    getline(cin, myName);
+    cout << "You typed: " << myName << endl;
+    cout << "Type in your name and age." << endl;
+    cin >> myName >> yourAge;
+    cout << "You typed: " << myName << " and " << yourAge << endl;
 }
